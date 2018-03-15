477e4a2904d70da59ac07230b96f34e16a0f1909
Daniel Rodriguez <dannyjose1112@hotmail.com>
2018-03-15 16:20:58 -0600

    [UPDATED] removed forced columns for mesas

--- app/Http/Controllers/MesasController.php
+++ app/Http/Controllers/MesasController.php
@@ -40,8 +40,6 @@ class MesasController extends Controller
     {
         $validator = Validator::make($request->all(), [
             'nombre'       => 'required',
-            'codigo'       => 'required',
-            'costo'        => 'required',
         ]);
         if ( $validator->fails() ) {
             $returnData = array (
